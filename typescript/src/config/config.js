c6468c0b5f71114c64bf5fab53491ae11b3500fb
aventi-sas <164091456+aventi-sas@users.noreply.github.com>
2024-04-09 22:35:32 +0000

    add types and remove interfaces no utils

--- typescript/src/config/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    "formNewTask": {
-        "input-1": "input[name='title-task']",
-        "input-2": "input[name='desc-task']",
-        "select-1": "select[name='type-select']"
-    }
-};
